7c65144290b7da49c03422e07047cdd72e541562
Ray <jr071100@gmail.com>
2023-05-04 17:31:25 +0700

    Created enemy and spawner

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+		Instantiate(enemyPrefab, GenerateSpawnPosition(), Quaternion.identity);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+		float spawnPosX = Random.Range(-spawnRange, spawnRange);
+		float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+		Vector3 spawnPosition = new Vector3(spawnPosX, 0, spawnPosZ);
+        return spawnPosition;
+	}
+}
